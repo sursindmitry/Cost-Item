ddc3a8c2317e5c58e9c41e23dd131a60a5ce4a41
sursindmitry <dmitrijsursin@gmail.com>
2023-03-11 18:23:14 +0700

    Card

--- src/components/Costs.js
+++ src/components/Costs.js
@@ -1,13 +1,14 @@
 import './Costs.css'
 import CostItem from "./CostItem";
+import Card from "./Card";
 
 function Costs(props){
     return(
-        <div className='costs'>
+        <Card className='costs'>
             <CostItem date={props.costs[0].date} description={props.costs[0].description} amount={props.costs[0].amount}/>
             <CostItem date={props.costs[1].date} description={props.costs[1].description} amount={props.costs[1].amount}/>
             <CostItem date={props.costs[2].date} description={props.costs[2].description} amount={props.costs[2].amount}/>
-        </div>
+        </Card>
     );
 }
 

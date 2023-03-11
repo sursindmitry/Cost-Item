455f7df85b070e964924a4431c36bc200be7b04b
sursindmitry <dmitrijsursin@gmail.com>
2023-03-11 18:02:23 +0700

    Costs

---
+++ src/components/Costs.js
@@ -0,0 +1,14 @@
+import './Costs.css'
+import CostItem from "./CostItem";
+
+function Costs(props){
+    return(
+        <div className='costs'>
+            <CostItem date={props.costs[0].date} description={props.costs[0].description} amount={props.costs[0].amount}/>
+            <CostItem date={props.costs[1].date} description={props.costs[1].description} amount={props.costs[1].amount}/>
+            <CostItem date={props.costs[2].date} description={props.costs[2].description} amount={props.costs[2].amount}/>
+        </div>
+    );
+}
+
+export default Costs;
